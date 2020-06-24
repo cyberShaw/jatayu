3b6d0b347881ebfc4846cb65430981e8248fa7c1
Saiharsha B <saibalsu@gmail.com>
2020-06-24 22:59:06 +0530

    Complete Dashboard with placeholders

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,11 +1,12 @@
 <template>
-    <aside class="menu">
+    <aside class="menu is-hidden-mobile">
         <p class="menu-label">
             General
         </p>
         <ul class="menu-list">
-            <li><a>Dashboard</a></li>
+            <li><a class="is-active">Dashboard</a></li>
             <li><a>Customers</a></li>
+            <li><a>Other</a></li>
         </ul>
         <p class="menu-label">
             Administration
@@ -13,16 +14,18 @@
         <ul class="menu-list">
             <li><a>Team Settings</a></li>
             <li>
-                <a class="is-active">Manage Your Team</a>
+                <a>Manage Your Team</a>
                 <ul>
                     <li><a>Members</a></li>
                     <li><a>Plugins</a></li>
                     <li><a>Add a member</a></li>
+                    <li><a>Remove a member</a></li>
                 </ul>
             </li>
             <li><a>Invitations</a></li>
             <li><a>Cloud Storage Environment Settings</a></li>
             <li><a>Authentication</a></li>
+            <li><a>Payments</a></li>
         </ul>
         <p class="menu-label">
             Transactions
@@ -31,13 +34,14 @@
             <li><a>Payments</a></li>
             <li><a>Transfers</a></li>
             <li><a>Balance</a></li>
+            <li><a>Reports</a></li>
         </ul>
     </aside>
 </template>
 
 <script>
 export default {
-    name: 'Aside',    
+    name: 'SideBar'
 }
 </script>
 
