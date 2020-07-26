86fae87bb3a8432400284bf72c861f0818905043
Saiharsha B <saibalsu@gmail.com>
2020-07-27 03:36:14 +0530

    photo and upload video pages, done

--- pages/upload/video.vue
+++ pages/upload/video.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="page">
     <PageHead title="Video" subtitle="Upload" />
     <b-message type="is-success" has-icon icon="arrow-circle-up" icon-pack="fas" size="is-medium">
       <p
@@ -8,7 +8,7 @@
     </b-message>
     <section>
       <div class="columns">
-        <div class="column is-5">
+        <div class="column is-5 anim">
           <lottie
             :options="defaultOptions"
             :height="400"
@@ -19,7 +19,7 @@
         <div class="column">
           <b-field class="file">
             <b-upload v-model="file" expanded>
-              <a class="button is-primary is-fullwidth">
+              <a class="button is-link is-fullwidth">
                 <b-icon icon="upload"></b-icon>
                 <span>{{ file.name || "Click to upload"}}</span>
               </a>
@@ -49,8 +49,8 @@
           </div>
           <div class="butn">
             <b-button
-              @click="uploadPhoto"
-              type="is-success"
+              @click.prevent="uploadVideo"
+              type="is-link"
               size="is-medium"
               rounded
               icon-left="cloud-upload-alt"
@@ -67,10 +67,22 @@
           </div>
           <br />
           <div>
-            <b-message
-              type="is-success"
-              has-icon
-            >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id fermentum quam. Proin sagittis, nibh id hendrerit imperdiet, elit sapien laoreet elit</b-message>
+            <b-message type="is-success" has-icon v-if="success==1">
+              <p
+                class="is-family-monospace has-text-weight-bold"
+              >Your video has been uploaded successfully. Thank you for contributing to the dataset! 😄</p>
+              <p>The uploaded file is of size -> {{ size }} MB</p>
+              <p>
+                It can be viewed at:
+                <a :href="bucketURL">{{ bucketURL }}</a>
+              </p>
+            </b-message>
+            <b-message type="is-danger" has-icon v-if="success==2">
+              <p
+                class="is-family-monospace has-text-weight-bold"
+              >There was an error while uploading the file! 🙃</p>
+              <p>The error status is: {{ error }}</p>
+            </b-message>
           </div>
         </div>
       </div>
@@ -94,8 +106,10 @@ export default {
       animationSpeed: 1,
       file: {},
       dropFiles: [],
-      status: '',
+      success: null,
       error: '',
+      bucketURL: '',
+      size: '',
     };
   },
   methods: {
@@ -105,19 +119,31 @@ export default {
     deleteDropFile(index) {
       this.dropFiles.splice(index, 1);
     },
-    uploadPhoto() {
-      console.log(this.file);
-      this.$axios
-        .post('https://upload-dimg.herokuapp.com/upload/image', this.file)
+    uploadVideo() {
+      // console.log(this.file);
+      let fd = new FormData();
+      fd.append('video', this.file);
+      this.$axios({
+        url: 'https://upload-dimg.herokuapp.com/upload/video',
+        headers: {
+          'Content-Type': 'video/mp4',
+        },
+        method: 'post',
+        data: fd,
+      })
         .then(res => {
           console.log(res);
+          this.success = 1;
+          this.bucketURL = res.data.url;
+          this.size = Math.round((res.data.size / (1024 * 1024) + Number.EPSILON) * 100) / 100;
         })
         .catch(err => {
-          this.error = res;
+          this.error = err;
+          this.success = 2;
         });
     },
     clearFiles() {
-      this.files = {};
+      this.file = {};
     },
   },
 };
@@ -127,5 +153,16 @@ export default {
 .butn {
   display: flex;
   justify-content: center;
+}
+.anim {
+  position: relative;
+}
+
+div > svg {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  left: 0;
+  top: 0;
 }
 </style>
