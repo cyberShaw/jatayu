82808e0e77323d37f6c06ff7c2587dd1b04ccc9d
Saiharsha B <saibalsu@gmail.com>
2020-07-28 17:00:43 +0530

    fix env variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,6 +54,17 @@ export default {
     '@/plugins/firebase.js',
     '@/plugins/fireauth.js',
   ],
+
+  env: {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  },
   /*
    ** Nuxt.js dev-modules
    */
