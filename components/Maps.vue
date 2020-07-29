4a03eaf6da655061a5dd51dabd59d897094ab461
Saiharsha B <saibalsu@gmail.com>
2020-07-29 23:45:00 +0530

    update Maps component

--- components/Maps.vue
+++ components/Maps.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    <iframe id="map-box" width="100%" height="" frameborder="0" style="border:0" allowfullscreen>
-    </iframe>
+    <p class="subtitle">Location of Detection</p>
+    <GMap
+      ref="gMap"
+      language="en"
+      :cluster="{options: {styles: clusterStyle}}"
+      :center="{lat: locations[0].lat, lng: locations[0].lng}"
+      :options="{fullscreenControl: false, styles: mapStyle}"
+      :zoom="6"
+    >
+      <GMapMarker
+        v-for="location in locations"
+        :key="location.id"
+        :position="{lat: location.lat, lng: location.lng}"
+        :options="{icon: location === currentLocation ? pins.selected : pins.notSelected}"
+        @click="currentLocation = location"
+      ></GMapMarker>
+    </GMap>
   </div>
 </template>
 
@@ -9,17 +24,36 @@
 export default {
   name: 'Maps',
   props: {
-    loc: '',
+    loc: [],
   },
   data() {
     return {
-      apiKey: 'AIzaSyC9U4sCMhib5oL3laji-Tlb7AJgKrioYEM',
-      rootQuery: 'https://www.google.com/maps/embed/v1/place?key=',
+      currentLocation: {
+        lat: parseInt(this.loc[0]),
+        lng: parseInt(this.loc[1]),
+      },
+      circleOptions: {},
+      locations: [
+        {
+          lat: parseInt(this.loc[0]),
+          lng: parseInt(this.loc[1]),
+        },
+      ],
+      pins: {},
+      mapStyle: [],
+      clusterStyle: [
+        {
+          url:
+            'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m1.png',
+          width: 56,
+          height: 56,
+          textColor: '#fff',
+        },
+      ],
     };
   },
   mounted() {
-    let locationQuery = this.rootQuery.concat(this.apiKey, '&q=', this.loc);
-    document.getElementById('map-box').setAttribute('src', locationQuery);
+    console.log(this.loc);
   },
 };
 </script>
