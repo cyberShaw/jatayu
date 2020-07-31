a21f962d952a0cca48f8ebe23581dc3937b69805
Saiharsha B <saibalsu@gmail.com>
2020-08-01 02:37:37 +0530

    add verified feature :fire:

--- components/DetectionsTable.vue
+++ components/DetectionsTable.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <modal-box
+      :is-active="isModalActive"
+      :trash-object-name="trashObjectName"
+      :tf="tf"
+      @confirm="trashConfirm"
+      @cancel="trashCancel"
+    />
     <b-table
       :loading="isLoading"
       :paginated="paginated"
@@ -14,6 +21,7 @@
       :sort-icon="sortIcon"
       :sort-icon-size="sortIconSize"
       :data="clients"
+      ref="table"
     >
       <template slot-scope="props">
         <b-table-column label="Detection ID" field="id" searchable sortable>
@@ -33,7 +41,8 @@
           <nuxt-link
             :to="{name: 'criminal-cid', params: {cid: props.row.cid}}"
             class="button is-small is-link has-text-weight-bold"
-          >{{ props.row.cid }}</nuxt-link>
+            >{{ props.row.cid }}</nuxt-link
+          >
         </b-table-column>
         <b-table-column label="Location: Coordinates" field="location" sortable centered>
           <template slot="header" slot-scope="{column}">
@@ -57,6 +66,35 @@
           </template>
           <span class="tag is-link">{{ props.row.time_stamp }}</span>
           <!-- {{ props.row.time_stamp }} -->
+        </b-table-column>
+        <b-table-column label="Verified" field="valid" searchable centered sortable>
+          <template slot="header" slot-scope="{column}">
+            <p class="is-family-monospace">{{ column.label }}</p>
+          </template>
+          <template v-if="props.row.valid">
+            <b-icon icon="check-circle" pack="fas" size="is-medium" type="is-success"></b-icon>
+          </template>
+          <template v-else>
+            <b-icon icon="times-circle" pack="fas" size="is-medium" type="is-danger"></b-icon>
+          </template>
+        </b-table-column>
+        <b-table-column custom-key="action-false" label="Mark as False Positive">
+          <div
+            class="buttons"
+            @click.prevent="trashModal(props.row.id, false)"
+            v-if="props.row.valid"
+          >
+            <b-icon type="is-danger" icon="exclamation" pack="fas" size="is-medium" />
+          </div>
+        </b-table-column>
+        <b-table-column custom-key="actions-true" label="Mark as Verified">
+          <div
+            class="buttons"
+            @click.prevent="trashModal(props.row.id, true)"
+            v-if="!props.row.valid"
+          >
+            <b-icon icon="clipboard-check" pack="fas" type="is-success" size="is-medium" />
+          </div>
         </b-table-column>
       </template>
 
@@ -92,11 +130,13 @@
 <script>
 import axios from 'axios';
 import Maps from '@/components/Maps';
+import ModalBox from '@/components/ModalBox';
 
 export default {
   name: 'DetectionsTable',
   components: {
     Maps,
+    ModalBox,
   },
   props: {
     dataUrl: {
@@ -111,6 +151,7 @@ export default {
   data() {
     return {
       active: false,
+      isModalActive: false,
       trashObject: null,
       clients: [],
       isStriped: true,
@@ -124,12 +165,15 @@ export default {
       sortIconSize: 'is-medium',
       lats: [],
       mapData: [],
+      trashID: '',
+      trashInfo: '',
+      tf: '',
     };
   },
   computed: {
     trashObjectName() {
       if (this.trashObject) {
-        return this.trashObject.name;
+        return this.trashObject;
       }
 
       return null;
@@ -163,9 +207,6 @@ export default {
               el['location'] = this.mapData[i];
               i = i + 1;
             });
-            // console.log(this.mapData);
-            // console.log(this.lats);
-            console.log(this.clients);
           }
         })
         .catch(e => {
@@ -178,16 +219,69 @@ export default {
     }
   },
   methods: {
-    trashModal(trashObject) {
+    trashModal(trashObject, tf) {
       this.trashObject = trashObject;
+      this.trashID = trashObject;
+      this.tf = tf;
       this.isModalActive = true;
     },
     trashConfirm() {
       this.isModalActive = false;
-      this.$buefy.snackbar.open({
-        message: 'Confirmed',
-        queue: false,
-      });
+      this.$axios
+        .get('https://coders-of-blaviken-api.herokuapp.com/api/detections/' + this.trashID)
+        .then(res => {
+          this.trashInfo = res.data.detections[0];
+          this.updateDetection();
+        })
+        .catch(err => {
+          console.log(err);
+          this.$buefy.snackbar.open({
+            message: 'Error!',
+            queue: false,
+          });
+        });
+    },
+    updateDetection() {
+      console.log(
+        this.trashID,
+        this.trashInfo.cid,
+        this.trashInfo.location,
+        this.trashInfo.rsrc,
+        this.trashInfo.time_stamp,
+        this.tf
+      );
+      let body =
+        'cid=' +
+        this.trashInfo.cid +
+        '&' +
+        'location=' +
+        this.trashInfo.location +
+        '&' +
+        'rsrc=' +
+        this.trashInfo.rsrc +
+        '&' +
+        'time_stamp=' +
+        this.trashInfo.time_stamp +
+        '&' +
+        'valid=' +
+        this.tf;
+      this.$axios
+        .put('https://coders-of-blaviken-api.herokuapp.com/api/detections/' + this.trashID, body)
+        .then(res => {
+          // console.log(res);
+          this.$buefy.snackbar.open({
+            message: 'Successfully marked as ' + this.tf,
+            queue: false,
+          });
+        })
+        .catch(err => {
+          console.log(err);
+          this.$buefy.snackbar.open({
+            duration: 3000,
+            message: 'Error!',
+            queue: false,
+          });
+        });
     },
     trashCancel() {
       this.isModalActive = false;
