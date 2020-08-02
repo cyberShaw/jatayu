c9d9750389ca929d14c00bb5d1f04fc7c4ad5a32
Saiharsha B <saibalsu@gmail.com>
2020-08-02 13:48:42 +0530

    add track feature :fire:

--- pages/track.vue
+++ pages/track.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="page">
     <PageHead subtitle="Detect & Track" title="Recents & Tracking" />
-    <Card class="has-table" title="Criminal Detections">
+    <Card class="has-table" title="Recent Criminal Detections" v-if="isLoaded">
       <GMap
         ref="gMap"
         language="en"
         :cluster="{options: {styles: clusterStyle}}"
-        :center="{lat: parseFloat(locations[0].lat), lng: parseFloat(locations[0].lng)}"
+        :center="{lat: locations[0].lat, lng: locations[0].lng}"
         :options="{fullscreenControl: false, styles: mapStyle}"
         :zoom="6"
       >
@@ -17,10 +17,21 @@
           :options="{icon: location === currentLocation ? pins.selected : pins.notSelected}"
           @click="currentLocation = location"
         >
-          <GMapInfoWindow :options="{maxWidth: 200}">
-            <code>
-              <p @click="openInNewWindow(location.lat, location.lng)">Open location in Google Maps</p>
-            </code>
+          <GMapInfoWindow :options="{maxWidth: 400}">
+            <b-message type="is-danger">
+              <p>
+                <span class="title is-family-primary is-size-6">Detection ID:</span>
+                <span class="subtitle is-family-primary is-size-6">{{ location.id }}</span>
+              </p>
+              <p>
+                <span class="title is-family-primary is-size-6">Criminal ID:</span>
+                <span class="subtitle is-family-primary is-size-6">{{ location.cid }}</span>
+              </p>
+              <b-button
+                @click="openInNewWindow(location.lat, location.lng)"
+                class="has-text-weight-centered is-family-primary"
+              >Track >></b-button>
+            </b-message>
           </GMapInfoWindow>
         </GMapMarker>
       </GMap>
@@ -43,47 +54,16 @@ export default {
         lat: '11.0168',
         lng: '76.9558',
       },
+      detections: [],
+      mapData: [],
+      locations: [],
+      clients: null,
+      lats: [],
       circleOptions: {},
-      locations: [
-        {
-          lat: '13.0168',
-          lng: '76.9558',
-        },
-        {
-          lat: '14.0268',
-          lng: '77.9158',
-        },
-        {
-          lat: '11.0268',
-          lng: '74.9108',
-        },
-        {
-          lat: '16.0268',
-          lng: '76.9158',
-        },
-        {
-          lat: '18.0168',
-          lng: '76.9158',
-        },
-        {
-          lat: '12.0268',
-          lng: '76.9108',
-        },
-        {
-          lat: '11.1268',
-          lng: '80.9158',
-        },
-        {
-          lat: '11.1268',
-          lng: '76.9160',
-        },
-        {
-          lat: '16.0268',
-          lng: '76.9158',
-        },
-      ],
+      locations: [],
       pins: {},
       mapStyle: [],
+      isLoaded: false,
       clusterStyle: [
         {
           url:
@@ -96,7 +76,57 @@ export default {
     };
   },
   mounted() {
-    // console.log(this.loc);
+    // this.isLoading = true;
+    this.$axios
+      .get('https://coders-of-blaviken-api.herokuapp.com/api/detections')
+      .then(r => {
+        this.isLoading = false;
+        if (r.data && r.data.detections) {
+          this.clients = r.data.detections;
+          this.clients.forEach(el => {
+            let val = el['location'];
+            this.lats.push(val);
+            // console.log(this.lats);
+          });
+          for (let i = 0; i < this.lats.length; i++) {
+            let res = this.lats[i].split('Lats');
+            this.mapData.push(res);
+            // console.log(this.mapData);
+          }
+          let i = 0;
+          this.clients.forEach(el => {
+            el['location'] = this.mapData[i];
+            i = i + 1;
+          });
+          // console.log(this.clients);
+        }
+        for (let i = 0; i < this.clients.length; i++) {
+          let temp = {
+            lat: '',
+            lng: '',
+            id: '',
+            cid: '',
+            timeStamp: '',
+          };
+          temp.lat = parseFloat(this.clients[i].location[0]);
+          temp.lng = parseFloat(this.clients[i].location[1]);
+          temp.id = this.clients[i].id;
+          temp.cid = this.clients[i].cid;
+          temp.timeStamp = this.clients[i].time_stamp;
+          this.locations.push(temp);
+        }
+        // this.locations.push({lat: 23.4356, lng: 23.4234, id: 23, cid: 1231});
+
+        // console.log(JSON.stringify(this.locations));
+        this.isLoaded = true;
+      })
+      .catch(e => {
+        this.isLoading = false;
+        this.$buefy.toast.open({
+          message: `Error: ${e.message}`,
+          type: 'is-danger',
+        });
+      });
   },
   methods: {
     openInNewWindow(lat, lng) {
